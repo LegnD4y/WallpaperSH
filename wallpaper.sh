2b6df240af2629a4cb563eb36a4c92ed13e6be38
zLegendEnd <gustavoolsilva11@gmail.com>
2023-08-29 22:56:39 -0300

    Finalização

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
+kill $(pidof mplayer)
 n=0
-
-for i in /home/gustavo/Pictures/Wallpapers/*.mp4
+for i in /home/gustavo/Imagens/Wallpapers/*.mp4
 do
   n=$((n+1))
 done
-array=($(ls /home/gustavo/Pictures/Wallpapers/*.mp4))
+array=($(ls /home/gustavo/Imagens/Wallpapers/*.mp4))
 while :
 do
   x=$((RANDOM%n))
-  t=ffmpeg -i ${array[$x]} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | cut -d ':' -f 3 | cut -d '.' -f 1 &
-  echo $t
-  kill $(pidof mplayer)
-  if [ "$t" -lt 20 ]
+  t=$($(ffmpeg -i ${array[$x]} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | cut -d ':' -f 3 | cut -d '.' -f 1) * 60)
+  t=$($t+$(ffmpeg -i ${array[$x]} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | cut -d ':' -f 3 | cut -d '.' -f 1))
+  if [ "$t" -lt 30 ]
   then
-    #mplayer -loop 0 -rootwin -ao null -noconsolecontrols -fs ${array[$x]} &
-    sleep 20
-  else 
-    #mplayer -rootwin -ao null -noconsolecontrols -fs ${array[$x]} &
-    sleep 20
+    mplayer -loop 0 -rootwin -ao null -noconsolecontrols -fs ${array[$x]} &
+    sleep 30
+    kill $(pidof mplayer)
+  else
+    if [ "$t" -gt 60 ]
+    then
+      mplayer -rootwin -ao null -noconsolecontrols -fs ${array[$x]} &
+      sleep 60
+      kill $(pidof mplayer)
+    else
+      mplayer -rootwin -ao null -noconsolecontrols -fs ${array[$x]} && kill $(pidof mplayer)
+    fi
   fi
 done
 kill $(pidof mplayer)

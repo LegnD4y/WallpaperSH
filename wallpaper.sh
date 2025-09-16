2824d53e1f2a166ea2a1a0fe52f21c6e2db581a9
Legend0b0 <gustavoolsilva11@gmail.com>
2025-09-16 09:15:10 -0300

    Update

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
-kill $(pidof mplayer)
+kill $(pidof xwinwrap)
 while :
 do
-  for i in $(ls $HOME/Imagens/Wallpapers/*.mp4 | shuf);
+  for i in $(ls $HOME/Videos/Wallpapers/*.mp4 | shuf);
   do
-    mplayer -loop 0 -rootwin -ao null -noconsolecontrols -fstype layer $i &
+    MOVX=0
+    I=0
+    YInit=0
+    
+    for j in $(xrandr|awk '/*/{print $1}');
+    do
+      arrIN=(${j//x/ })
+
+      if [ $I -eq 0 ]; 
+      then
+        YInit=${arrIN[1]}
+      fi
+
+      MOVY=$((YInit-arrIN[1]))
+      xwinwrap -ov -g ${arrIN[0]}x${arrIN[1]}+$MOVX+$MOVY -- mpv -wid WID $i --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-input-default-bindings &
+      echo xwinwrap -ov -g ${arrIN[0]}x${arrIN[1]}+$MOVX+$MOVY -- mpv -wid WID $i --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-input-default-bindings &
+      MOVX=$((MOVX+arrIN[0]))
+      I=$((I+1))
+    done
+
     sleep 30
-    kill $(pidof mplayer)
+    echo antes do kill
+    kill $(pidof xwinwrap)
+    echo depois do kill
   done
 done
-kill $(pidof mplayer)
+kill $(pidof xwinwrap)
